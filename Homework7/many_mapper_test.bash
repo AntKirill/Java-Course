839b3c7ab0303d869d878f1efec167343e34e37c
kant97 <kirant9797@gmail.com>
2017-04-10 09:29:27 +0300

    cool

--- Homework7/many_mapper_test.bash
+++ Homework7/many_mapper_test.bash
@@ -1,6 +1,7 @@
 javac -d production/java-advanced-2017/ src/ru/ifmo/ctddev/antonov/concurrent/*.java src/info/kgeorgiy/java/advanced/mapper/*.java src/info/kgeorgiy/java/advanced/concurrent/*.java;
 
-for ((i = 0; i < 6; i++))
+K=6;
+for ((i = 0; i < K; i++))
 do
 	java -cp artifacts/*:lib/*:./production/java-advanced-2017/ info.kgeorgiy.java.advanced.mapper.Tester list ru.ifmo.ctddev.antonov.concurrent.ParallelMapperImpl,ru.ifmo.ctddev.antonov.concurrent.IterativeParallelism
 	if [ $? -eq 1 ] 
