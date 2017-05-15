9896ba9e98dc285dbefa6766b01188f231621e40
kant97 <kirant9797@gmail.com>
2017-05-15 15:41:15 +0300

    server works

---
+++ Homework9/hello_client.bash
@@ -0,0 +1,3 @@
+javac -d production/Java-Course/ -cp artifacts/HelloUDPTest.jar:lib/*:./ src/ru/ifmo/ctddev/antonov/hello/*.java java/info/kgeorgiy/java/advanced/hello/*.java;
+
+java -cp artifacts/HelloUDPTest.jar:lib/*:./production/Java-Course/ info.kgeorgiy.java.advanced.hello.Tester client ru.ifmo.ctddev.antonov.hello.HelloUDPClient
---
+++ Homework9/src/ru/ifmo/ctddev/antonov/hello/HelloUDPServer.java
@@ -0,0 +1,68 @@
+package ru.ifmo.ctddev.antonov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloServer;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.nio.charset.Charset;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class HelloUDPServer implements HelloServer {
+
+    private static final Charset charset = Charset.forName("UTF-8");
+    private DatagramSocket socket;
+    private ExecutorService threadPool;
+    private volatile boolean isRunnig = true;
+
+    /**
+     *
+     */
+    @Override
+    public void start(int port, int threads) {
+        try {
+            socket = new DatagramSocket(port);
+        } catch (SocketException e) {
+            //System.out.print("Can not create socket!\n");
+            //System.out.println(e.getMessage());
+        }
+        threadPool = Executors.newFixedThreadPool(threads);
+
+        for (int i = 0; i < threads; i++) {
+            threadPool.submit(() -> {
+                while (isRunnig) {
+                    DatagramPacket query = null;
+                    try {
+                        query = new DatagramPacket((new byte[socket.getReceiveBufferSize()]), socket.getReceiveBufferSize());
+                    } catch (SocketException e) {
+                        //System.out.println("Unable to get query!");
+                        //System.out.println(e.getMessage());
+                    }
+
+                    try {
+                        socket.receive(query);
+                        byte[] resp = ("Hello, ".concat(new String(query.getData(), 0, query.getLength()))).getBytes(charset);
+                        DatagramPacket response = new DatagramPacket(resp, resp.length, query.getAddress(), query.getPort());
+                        socket.send(response);
+                    } catch (IOException e) {
+                        //e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void close() {
+        synchronized (this) {
+            isRunnig = false;
+            socket.close();
+            threadPool.shutdown();
+        }
+    }
+}
